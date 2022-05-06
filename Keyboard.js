57657151d76914ec629e2e8dccc8a413751b28ff
daniscrail <cap3347@mail.ru>
2022-05-06 20:04:52 +0300

    feat: update code

--- Keyboard.js
+++ Keyboard.js
@@ -47,27 +47,44 @@ for (let key of keyLayoutEng) {
 }
 
 let keys = document.querySelectorAll("button");
-let space = document.querySelector(".btn_space")
+let space = document.querySelector(".btn_space");
+let count_CL = 1;
 
 for (let button of keys) {
     button.addEventListener("click", function () {
+        var posCursor = this.selectionEnd;
+
         if (this.innerHTML !== "Tab" && this.innerHTML !== "CapsLock" && this.innerHTML !== "Shift" &&
             this.innerHTML !== "Ctrl" && this.innerHTML !== "Alt" && this.innerHTML !== "Enter" &&
-            this.innerHTML !== "Backspace" && this.innerHTML !== "Win" && this.innerHTML !== "Del" && this !== space) {
-            textarea.value += this.innerHTML.toLowerCase();
+            this.innerHTML !== "Backspace" && this.innerHTML !== "Win" && this.innerHTML !== "Delete" && this !== space) {
+            if (count_CL % 2) {
+                textarea.value += this.innerHTML.toLowerCase();
+            } else {
+                textarea.value += this.innerHTML.toUpperCase();
+            }
         } else if (this.innerHTML === "Backspace") {
-            textarea.value = textarea.value.slice(0, textarea.innerHTML.length - 1);
+            if (textarea.selectionStart > 0) {
+                textarea.value = textarea.value.slice(0, textarea.selectionStart - 1) + textarea.value.slice(textarea.selectionStart, textarea.value.length);
+            } else {
+                textarea.value = textarea.value.slice(0, textarea.value.length - 1);
+                console.log(textarea.selectionStart);
+            }
         } else if (this.innerHTML === "Enter") {
             textarea.value = textarea.value + "\n";
         } else if (this.innerHTML.length === 0) {
             textarea.value = textarea.value + " ";
         } else if (this === space) {
-            textarea.value = textarea.value + " ";
+            textarea.value = textarea.value.slice(0, posCursor) + " " + textarea.value;
         } else if (this.innerHTML === "Tab") {
             textarea.value = textarea.value + " " + " " + " " + " ";
+        } else if (this.innerHTML === "Delete") {
+            textarea.value = textarea.value.slice(0, textarea.selectionStart) + textarea.value.slice(textarea.selectionStart + 1, textarea.value.length);
+        } else if (this.innerHTML === "CapsLock") {
+            count_CL++;
         }
     });
 }
+
 
 textarea.addEventListener("keydown", function (event) {
     let str = event.key;
@@ -84,36 +101,45 @@ textarea.addEventListener("keydown", function (event) {
     }
 
     for (let elem of keys) {
+
         if (elem.innerHTML.toUpperCase() === str.toUpperCase()) {
-            elem.style.background = "wheat";
+            elem.classList.remove("btn_keyboard_inactive");
+            elem.classList.add("btn_keyboard_active");
         }
 
         if (elem.innerHTML === "Ctrl" && str === "Control") {
-            elem.style.background = "wheat";
+            elem.classList.remove("btn_keyboard_inactive");
+            elem.classList.add("btn_keyboard_active");
         }
 
         if (str === "ArrowRight" && elem.innerHTML === "→") {
-            elem.style.background = "wheat";
+            elem.classList.remove("btn_keyboard_inactive");
+            elem.classList.add("btn_keyboard_active");
         }
 
         if (str === "ArrowLeft" && elem.innerHTML === "←") {
-            elem.style.background = "wheat";
+            elem.classList.remove("btn_keyboard_inactive");
+            elem.classList.add("btn_keyboard_active");
         }
 
         if (str === "ArrowDown" && elem.innerHTML === "↓") {
-            elem.style.background = "wheat";
+            elem.classList.remove("btn_keyboard_inactive");
+            elem.classList.add("btn_keyboard_active");
         }
 
         if (str === "ArrowUp" && elem.innerHTML === "↑") {
-            elem.style.background = "wheat";
+            elem.classList.remove("btn_keyboard_inactive");
+            elem.classList.add("btn_keyboard_active");
         }
 
         if (str === "Meta" && elem.innerHTML === "Win") {
-            elem.style.background = "wheat";
+            elem.classList.remove("btn_keyboard_inactive");
+            elem.classList.add("btn_keyboard_active");
         }
 
-        if (event.code === "Space" && elem.innerHTML === " ") {
-            elem.style.background = "wheat";
+        if (elem.classList.contains("btn_space") && str === " ") {
+            elem.classList.remove("btn_space");
+            elem.classList.add("btn_space_active");
         }
 
         if (str === "Tab" && elem.innerHTML === "Tab") {
@@ -126,37 +152,48 @@ textarea.addEventListener("keyup", function (event) {
     let str = event.key;
 
     for (let elem of keys) {
-        if (elem.style.background === "wheat" &&
+        if (elem.classList.contains("btn_keyboard_active") &&
             str.toUpperCase() === elem.innerHTML.toUpperCase()) {
-            elem.style.background = "rgb(44, 41, 41)";
+            elem.classList.remove("btn_keyboard_active");
+            elem.classList.add("btn_keyboard_inactive");
         }
-        if (str === "ArrowRight" && elem.innerHTML === "→" && elem.style.background === "wheat") {
-            elem.style.background = "rgb(44, 41, 41)";
-        }
-
-        if (str === "ArrowLeft" && elem.innerHTML === "←" && elem.style.background === "wheat") {
-            elem.style.background = "rgb(44, 41, 41)";
+        if (str === "ArrowRight" && elem.innerHTML === "→" && elem.classList.contains("btn_keyboard_active")) {
+            elem.classList.remove("btn_keyboard_active");
+            elem.classList.add("btn_keyboard_inactive");
         }
 
-        if (str === "ArrowDown" && elem.innerHTML === "↓" && elem.style.background === "wheat") {
-            elem.style.background = "rgb(44, 41, 41)";
+        if (str === "ArrowLeft" && elem.innerHTML === "←" && elem.classList.contains("btn_keyboard_active")) {
+            elem.classList.remove("btn_keyboard_active");
+            elem.classList.add("btn_keyboard_inactive");
         }
 
-        if (str === "ArrowUp" && elem.innerHTML === "↑" && elem.style.background === "wheat") {
-            elem.style.background = "rgb(44, 41, 41)";
+        if (str === "ArrowDown" && elem.innerHTML === "↓" && elem.classList.contains("btn_keyboard_active")) {
+            elem.classList.remove("btn_keyboard_active");
+            elem.classList.add("btn_keyboard_inactive");
+        }
+
+        if (str === "ArrowUp" && elem.innerHTML === "↑" && elem.classList.contains("btn_keyboard_active")) {
+            elem.classList.remove("btn_keyboard_active");
+            elem.classList.add("btn_keyboard_inactive");
         }
 
         if (elem.innerHTML === "Ctrl" && str === "Control") {
-            elem.style.background = "rgb(44, 41, 41)";
+            elem.classList.remove("btn_keyboard_active");
+            elem.classList.add("btn_keyboard_inactive");
         }
 
         if (elem.innerHTML === "Tab" && str === "Tab") {
-            elem.style.background = "rgb(44, 41, 41)";
+            elem.classList.remove("btn_keyboard_active");
+            elem.classList.add("btn_keyboard_inactive");
         }
 
-        if (str === "Tab"){
+        if (elem.classList.contains("btn_space_active")) {
+            elem.classList.remove("btn_space_active");
+            elem.classList.add("btn_space");
+        }
+
+        if (str === "Tab") {
             event.preventDefault()
         }
     }
 });
-
